3028052634b805f8f2ed9966354891d807db91b6
Documenter.jl <documenter@juliadocs.github.io>
2021-09-29 01:09:11 +0000

    build based on a0669b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BNPRegressionGGA2021","category":"page"},{"location":"#BNPRegressionGGA2021","page":"Home","title":"BNPRegressionGGA2021","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BNPRegressionGGA2021.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BNPRegressionGGA2021]","category":"page"}]
+}

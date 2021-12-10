ff1e7537717cb8bba5569b04607c786b5df7966b
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-12-10 17:57:56 +0000

    Add a bigger example

--- test/sandbox.jl
+++ test/sandbox.jl
@@ -151,6 +151,34 @@ plot(x = y0, color = X0concat, Geom.density)
 plot(x = y1, y = fb, color = X1concat, Geom.line)
 mean(chainβ)
 
+#==================================================#
+# Example 5 - 10 predictors (mixture distribution) #
+#==================================================#
+
+# Simulate a sample 
+function simulate_sample(rng, N0, N1)
+    dϵ = MixtureModel(Normal, [(1.0, 1.0), (-1.0, 0.25)], [0.5, 0.5])
+    x0 = [-0.5, 0.5]
+    X0 = [ones(N0) rand(rng, x0, N0, 10)]
+    y0 = X0 * [1; zeros(9); 1] + rand(rng, dϵ, N0)
+    y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 2)
+    x1 = kron([x0[1], x0[2]], ones(N1))
+    X1 = [ones(2 * N1) x1 ones(2 * N1, 9)]
+    return y0, X0, y1, X1, r0
+end
+
+N0, N1 = 1000, 50
+rng = MersenneTwister(1);
+y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
+chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
+
+fb = mean(chainf);
+X0concat = @. string(X0[:, 2]) * " " * string(X0[:, 3]) * " " * string(X0[:, 4]);
+X1concat = @. string(X1[:, 2]) * " " * string(X1[:, 3]) * " " * string(X1[:, 4]);
+plot(x = y0, color = X0concat, Geom.density)
+plot(x = y1, y = fb, color = X1concat, Geom.line)
+mean([chainβ[i] .== zeros(11) for i in 1:length(chainβ)])
 
 # #=========================#
 # # Example 2 - 1 predictor, now using a mixture #

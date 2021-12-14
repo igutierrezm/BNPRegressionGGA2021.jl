96bbb02b610e4abc61fc04755452237f61a7d499
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-12-14 02:54:16 +0000

    e

--- test/sandbox.jl
+++ test/sandbox.jl
@@ -36,9 +36,9 @@ plot(
     layer(x = y0, Geom.histogram(density = true), color=["hist"])
 )
 
-#===============================================#
-# Example 2 - 1 predictor (simple distribution) #
-#===============================================#
+#========================================================#
+# Example 2 - 1 discrete predictor (simple distribution) #
+#========================================================#
 
 # Simulate a sample 
 function simulate_sample(rng, N0, N1)
@@ -48,12 +48,12 @@ function simulate_sample(rng, N0, N1)
     y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 2)
     x1 = [x0[1] * ones(N1); x0[2] * ones(N1)]
     X1 = [ones(2 * N1) x1]
-    return y0, X0, y1, X1, r0
+    return y0, X0, y1, X1
 end
 
 N0, N1 = 1000, 50
 rng = MersenneTwister(1);
-y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
 smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
 chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
 
@@ -62,9 +62,9 @@ plot(x = y0, color = X0[:, 2], Geom.density)
 plot(x = y1, y = fb, color = X1[:, 2], Geom.line)
 mean(chainβ)
 
-#================================================#
-# Example 3 - 1 predictor (mixture distribution) #
-#================================================#
+#=========================================================#
+# Example 3 - 1 discrete predictor (mixture distribution) #
+#=========================================================#
 
 # Simulate a sample 
 function simulate_sample(rng, N0, N1)
@@ -75,12 +75,12 @@ function simulate_sample(rng, N0, N1)
     y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 2)
     x1 = [x0[1] * ones(N1); x0[2] * ones(N1)]
     X1 = [ones(2 * N1) x1]
-    return y0, X0, y1, X1, r0
+    return y0, X0, y1, X1
 end
 
 N0, N1 = 1000, 50
 rng = MersenneTwister(1);
-y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
 smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
 chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
 
@@ -104,12 +104,12 @@ function simulate_sample(rng, N0, N1)
     x1 = kron([x0[1], x0[2], x0[1], x0[2]], ones(N1))
     x2 = kron([x0[1], x0[1], x0[2], x0[2]], ones(N1))
     X1 = [ones(4 * N1) x1 x2]
-    return y0, X0, y1, X1, r0
+    return y0, X0, y1, X1
 end
 
 N0, N1 = 1000, 50
 rng = MersenneTwister(1);
-y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
 smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
 chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
 
@@ -135,12 +135,12 @@ function simulate_sample(rng, N0, N1)
     x2 = kron([x0[1], x0[1], x0[2], x0[2], x0[1], x0[1], x0[2], x0[2]], ones(N1))
     x3 = kron([x0[1], x0[2], x0[1], x0[2], x0[1], x0[2], x0[1], x0[2]], ones(N1))
     X1 = [ones(8 * N1) x1 x2 x3]
-    return y0, X0, y1, X1, r0
+    return y0, X0, y1, X1
 end
 
 N0, N1 = 1000, 50
 rng = MersenneTwister(1);
-y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
 smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
 chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
 
@@ -164,12 +164,12 @@ function simulate_sample(rng, N0, N1)
     y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 2)
     x1 = kron([x0[1], x0[2]], ones(N1))
     X1 = [ones(2 * N1) x1 ones(2 * N1, 9)]
-    return y0, X0, y1, X1, r0
+    return y0, X0, y1, X1
 end
 
 N0, N1 = 1000, 50
 rng = MersenneTwister(1);
-y0, X0, y1, X1, r0 = simulate_sample(rng, N0, N1);
+y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
 smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
 chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
 mean([chainβ[i] .== zeros(11) for i in 1:length(chainβ)])
@@ -247,3 +247,56 @@ mean([chainβ[i] .== zeros(11) for i in 1:length(chainβ)])
 # f1 = mean(chainf);
 # plot(x = y0, color = x0, Geom.density)
 # plot(x = y1, y = f1, color = X1[:, 2], Geom.line)
+
+
+# #===========================================================#
+# # Example 4 - 1 continuous predictor (mixture distribution) #
+# #===========================================================#
+
+# # Simulate a sample 
+# function simulate_sample(rng, N0, N1)
+#     dϵ = MixtureModel(Normal, [(1.0, 1.0), (-1.0, 0.25)], [0.5, 0.5])
+#     x0 = [-2, 0, 2]
+#     X0 = [ones(N0) randn(rng, N0)]
+#     y0 = X0 * [0, 1] + 0.25 * rand(rng, dϵ, N0)
+#     y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 3)
+#     x1 = kron(x0, ones(N1))
+#     X1 = [ones(3 * N1) x1]
+#     return y0, X0, y1, X1
+# end
+
+# N0, N1 = 1000, 50
+# rng = MersenneTwister(1);
+# y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
+# smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
+# chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 20000, burnin = 10000);
+
+# fb = mean(chainf);
+# plot(x = y1, y = fb, color = X1[:, 2], Geom.line)
+# mean(chainβ)
+
+# #==========================================================#
+# # Example 3 - 1 continuous predictor (simple distribution) #
+# #==========================================================#
+
+# # Simulate a sample 
+# function simulate_sample(rng, N0, N1)
+#     x0 = [-2, 0, 2]
+#     X0 = [ones(N0) randn(rng, N0)]
+#     y0 = X0 * [0, 1] + 0.25 * randn(rng, N0)
+#     y1 = LinRange(minimum(y0), maximum(y0), N1) |> collect |> x -> repeat(x, 3)
+#     x1 = kron(x0, ones(N1))
+#     X1 = [ones(3 * N1) x1]
+#     return y0, X0, y1, X1
+# end
+
+# N0, N1 = 2000, 50
+# rng = MersenneTwister(1);
+# y0, X0, y1, X1 = simulate_sample(rng, N0, N1);
+# smpl = BNPRegressionGGA2021.Sampler(; y0, X0, y1, X1);
+# chainf, chainβ = BNPRegressionGGA2021.sample(rng, smpl; mcmcsize = 10000, burnin = 5000);
+
+# fb = mean(chainf);
+# # plot(x = y0, color = X0[:, 2], Geom.density)
+# plot(x = y1, y = fb, color = X1[:, 2], Geom.line)
+# mean(chainβ)

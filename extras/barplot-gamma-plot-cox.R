0200e4432f6467b90a1bffa6d61ce5c1f36fa815
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-01-11 03:44:17 +0000

    Add more plots relating cox model

--- extras/barplot-gamma-plot-cox.R
+++ extras/barplot-gamma-plot-cox.R
@@ -5,6 +5,7 @@ library(purrr)
 library(survival)
 library(svglite)
 library(glmulti)
+library(ggpubr)
 
 # Simulate a dataset from the DGP described in 5.2
 simulate_sample <- function(D0, N0) {
@@ -26,8 +27,22 @@ simulate_sample <- function(D0, N0) {
   return(df)
 }
 
+get_true_beta5 <- function() {
+    z0 <- seq(0, 6, length.out = 50)
+    f0 <- dlnorm(z0, 0.3, 0.7)
+    F0 <- plnorm(z0, 0.3, 0.7)
+    f1 <- 0.6 * dlnorm(z0, 0.0,  1.0) + 0.4 * dlnorm(z0, 0.3,  1.2)
+    F1 <- 0.6 * plnorm(z0, 0.0,  1.0) + 0.4 * plnorm(z0, 0.3,  1.2)
+    h0 <- f0 / (1 - F0)
+    h1 <- f1 / (1 - F1)
+    b5 <- log(h1 / h0)
+    return(b5)    
+}
+
+b5 <- get_true_beta5()
+
 simulate_test_results <- function(filename) {
-    data <- readr::read_csv(filename) |>
+    data <- readr::read_csv(filename, show_col_types = FALSE) |>
         dplyr::mutate(
             time = Surv(time = pmin(z0, c0), event = z0 < c0),
             across(starts_with("x"), as.factor)
@@ -92,3 +107,49 @@ p
 "barplot-gamma-cox.svg" |>
     ggsave(plot = p, width = 5 * 8 / 9, height = 3)
 
+#===============================================================================
+# Create the second plot for b5
+#===============================================================================
+
+simulate_test_results_2 <- function(filename) {
+    data <- readr::read_csv(filename, show_col_types = FALSE) |>
+        dplyr::mutate(
+            time = Surv(time = pmin(z0, c0), event = z0 < c0),
+            across(starts_with("x"), as.factor)
+        ) |>
+        dplyr::select(-c(x1, z0, c0)) |>
+        dplyr::rename_with(toupper, starts_with("x"))
+    cox_fit <- coxph(time ~ X2 + X3 + X4 + X5 + X6, data = data)
+    b5 <- 
+        broom::tidy(cox_fit) |>
+        dplyr::filter(term == "X61") |>
+        dplyr::pull(estimate)
+    return(b5)
+}
+
+results <-
+    list.files("data-survival-input-data", full.names = TRUE) |>
+    purrr::map_dbl(simulate_test_results_2)
+
+p_b5_fitted <- 
+    data.frame(x = 1:100, y = results) |>
+    ggplot(aes(x = x, y = y)) +
+    geom_point() +
+    theme_classic() + 
+    labs(x = "iter", y = "$\\hat{\\beta}_5$")
+
+p_b5_true <- 
+    data.frame(x = seq(0, 6, length.out = 50), y = get_true_beta5()) |>
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    theme_classic() +
+    labs(x = "$z_i$", y = "log-hazard ratio for $x_{i5}$ (1 vs 0)")
+
+p_b5_fitted
+p_b5_true
+combined_plot <-
+    ggarrange(p_b5_fitted, p_b5_true, labels = c("A", "B"), ncol = 2, nrow = 1)
+
+# Save the plot in svg format
+"barplot-gamma-cox-b5.svg" |>
+    ggsave(plot = combined_plot, width = 5 * 8 / 9, height = 3)
--- extras/barplot-gamma.jl
+++ extras/barplot-gamma.jl
@@ -1,61 +1,61 @@
-using Revise
+# using Revise
 
-using BNPRegressionGGA2021
-using CSV
-using DataStructures
-using DataFrames
-using Distributions
-using Gadfly
-using Random
-using Statistics
+# using BNPRegressionGGA2021
+# using CSV
+# using DataStructures
+# using DataFrames
+# using Distributions
+# using Gadfly
+# using Random
+# using Statistics
 
-const BNP = BNPRegressionGGA2021
+# const BNP = BNPRegressionGGA2021
 
-#====================================================#
-# Example 5 - 1 non-trivial and 4 trivial predictors #
-#====================================================#
+# #====================================================#
+# # Example 5 - 1 non-trivial and 4 trivial predictors #
+# #====================================================#
 
-function simulate_sample(N0, N1)
-    dy1 = LogNormal(0.3, 0.7);
-    dy2 = MixtureModel(LogNormal, [(0.0, 1.0), (0.3, 1.2)], [0.6, 0.4]);    
-    X0 = [ones(N0) rand([0, 1], N0, 5)]
-    X1 = kron([ones(2, 5) [1, 0]], ones(N1))
-    y0 = [X0[i, end] == 1 ? rand(dy2) : rand(dy1) for i in 1:N0]
-    c0 = rand(Exponential(10), N0)
-    y1 = LinRange(0, 6, N1) |> collect |> x -> repeat(x, size(X1, 1) ÷ N1)
-    z0 = deepcopy(y0)
-    return dy1, dy2, z0, c0, y0, X0, y1, X1
-end
+# function simulate_sample(N0, N1)
+#     dy1 = LogNormal(0.3, 0.7);
+#     dy2 = MixtureModel(LogNormal, [(0.0, 1.0), (0.3, 1.2)], [0.6, 0.4]);    
+#     X0 = [ones(N0) rand([0, 1], N0, 5)]
+#     X1 = kron([ones(2, 5) [1, 0]], ones(N1))
+#     y0 = [X0[i, end] == 1 ? rand(dy2) : rand(dy1) for i in 1:N0]
+#     c0 = rand(Exponential(10), N0)
+#     y1 = LinRange(0, 6, N1) |> collect |> x -> repeat(x, size(X1, 1) ÷ N1)
+#     z0 = deepcopy(y0)
+#     return dy1, dy2, z0, c0, y0, X0, y1, X1
+# end
 
-Random.seed!(1);
-N0, N1, Niter = 500, 2, 100;
-df = zeros(Bool, Niter, 6);
-for iter in 1:Niter
-    dy1, dy2, z0, c0, y0, X0, y1, X1 = simulate_sample(N0, N1);
-    m = BNP.DGPMErlang(; c0, y0, X0, y1, X1);
-    _, _, _, chaing = BNP.sample!(m; mcmcsize = 10000, burnin = 5000);
-    df[iter, :] .= begin
-        chaing |>
-        DataStructures.counter |>
-        collect |>
-        x -> sort(x, by = x -> x[2], rev = true) |>
-        x -> getindex(x, 1) |>
-        x -> getindex(x, 1)
-    end
-end
+# Random.seed!(1);
+# N0, N1, Niter = 500, 2, 100;
+# df = zeros(Bool, Niter, 6);
+# for iter in 1:Niter
+#     dy1, dy2, z0, c0, y0, X0, y1, X1 = simulate_sample(N0, N1);
+#     m = BNP.DGPMErlang(; c0, y0, X0, y1, X1);
+#     _, _, _, chaing = BNP.sample!(m; mcmcsize = 10000, burnin = 5000);
+#     df[iter, :] .= begin
+#         chaing |>
+#         DataStructures.counter |>
+#         collect |>
+#         x -> sort(x, by = x -> x[2], rev = true) |>
+#         x -> getindex(x, 1) |>
+#         x -> getindex(x, 1)
+#     end
+# end
 
-CSV.write("data/barplot-gamma.csv", DataFrame(df, :auto))
+# CSV.write("data/barplot-gamma.csv", DataFrame(df, :auto))
 
-#========================================================#
-# Repeat the experiment, but save the input samples only #
-#========================================================#
+# #========================================================#
+# # Repeat the experiment, but save the input samples only #
+# #========================================================#
 
-Random.seed!(1);
-N0, N1, Niter = 500, 2, 100;
-for iter in 1:Niter
-    dy1, dy2, z0, c0, y0, X0, y1, X1 = simulate_sample(N0, N1);
-    df = DataFrame(X0, :auto)
-    df[!, :z0] = z0
-    df[!, :c0] = c0
-    CSV.write("data/barplot-gamma-input-data-$iter.csv", df)
-end
+# Random.seed!(1);
+# N0, N1, Niter = 500, 2, 100;
+# for iter in 1:Niter
+#     dy1, dy2, z0, c0, y0, X0, y1, X1 = simulate_sample(N0, N1);
+#     df = DataFrame(X0, :auto)
+#     df[!, :z0] = z0
+#     df[!, :c0] = c0
+#     CSV.write("data/barplot-gamma-input-data-$iter.csv", df)
+# end

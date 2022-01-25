43fff3f985db263001042f4742533c34e706147e
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-01-25 14:05:05 +0000

    Put x_i in bold in dgpm-weights.R

--- extras/dgpm-weights.R
+++ extras/dgpm-weights.R
@@ -11,7 +11,7 @@ df <-
     ) |>
     dplyr::mutate(
         w = 1 / j * choose(j + s - 2, j - 1) * p^s * (1 - p)^(j - 1) * Re(hypergeo(j + s - 1, 1, j + 1, 1 - p)),
-        p_str = paste0("$\\varphi_j(x_i) = ", p, "$"),
+        p_str = paste0("$\\varphi(\\bm{x}_i) = ", p, "$"),
         s_str = as.character(s)
     )
 
@@ -21,7 +21,7 @@ p <-
     ggplot(aes(x = j, y = w, group = s_str, linetype = s_str)) + 
     facet_grid(cols = vars(p_str)) +
     geom_line() +
-    labs(x = "$j$", y = "$w_j(x_i)$", linetype = "s") +
+    labs(x = "$j$", y = "$w_j(\\bm{x}_i)$", linetype = "s") +
     theme_classic() +
     theme(legend.position = "top")
 p

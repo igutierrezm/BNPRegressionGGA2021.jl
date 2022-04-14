c8ffd522a2d28357c813bf81d6852fc56a865271
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-04-14 16:44:22 +0000

    Run the survival example

--- test/final/heatmap-survival.jl
+++ test/final/heatmap-survival.jl
@@ -43,16 +43,16 @@ function run_experiment_bnp(dy; N0, Nrep, Niter, id, filename)
     CSV.write(filename, reduced_gammas) # csv file
 end
 
-# function run_experiment_freq(dy; N0, Nrep, Niter, id, filename)
-#     Random.seed!(1) # seed
-#     samples = [generate_sample(dy; N0, Nrep) for _ in 1:Niter] # data
-#     gammas = [get_gamma_freq(samples[id]) for id in 1:Niter] # map gamma
-#     reduced_gammas = reduce_gammas(gammas) # reduced df
-#     reduced_gammas[!, :method] .= "freq"
-#     reduced_gammas[!, :id] .= id
-#     reduced_gammas[!, :N0] .= N0
-#     CSV.write(filename, reduced_gammas) # csv file
-# end
+function run_experiment_freq(dy; N0, Nrep, Niter, id, filename)
+    Random.seed!(1) # seed
+    samples = [generate_sample(dy; N0, Nrep) for _ in 1:Niter] # data
+    gammas = [get_gamma_freq(samples[id]) for id in 1:Niter] # map gamma
+    reduced_gammas = reduce_gammas(gammas) # reduced df
+    reduced_gammas[!, :method] .= "freq"
+    reduced_gammas[!, :id] .= id
+    reduced_gammas[!, :N0] .= N0
+    CSV.write(filename, reduced_gammas) # csv file
+end
 
 function generate_sample(dy; N0 = 50, Nrep = 10)
     # Simulate the data
@@ -123,10 +123,11 @@ end
 
 # Estimate gamma using a frequentist alternative
 function get_gamma_freq(sample)
-    y0, x0, y1, x1, X0, X1, f1, mapping = sample
+    y0, event0, x0, y1, x1, X0, X1, f1, mapping = sample
     R"""
     library(dplyr)
     data = data.frame(
+        status = $event0,
         y = $y0,
         x2 = $x0, 
         x3 = $X0[, 8],
@@ -134,10 +135,15 @@ function get_gamma_freq(sample)
         x5 = $X0[, 10],
         x6 = $X0[, 11]
     )
-    fitted_model <- leaps::regsubsets(y ~ ., data = data, nvmax = 5)
-    fitsum <- summary(fitted_model, matrix.logical = TRUE)
-    kstar <- which.min(fitsum$bic)
-    best_gamma <- fitsum$which[kstar, ]
+    fitted_model <- survival::coxph(survival::Surv(y, status) ~ ., data = data)
+    fitted_bestmodel <- MASS::stepAIC(fitted_model)
+    best_model_trues <- 
+        names(fitted_bestmodel$coefficients) %>%
+        gsub("x", "", .) %>%
+        as.numeric()
+    best_gamma <- rep(FALSE, 6)
+    best_gamma[best_model_trues] <- TRUE
+    best_gamma[1] <- TRUE
     """
     @rget best_gamma
     return best_gamma
@@ -157,12 +163,12 @@ begin
     run_experiment_bnp(dy; N0 = 50, Nrep = 5, Niter = 100, id = 1, filename)
 end 
 
-# # Experiment 1, N0 = 50 (freq)
-# begin 
-#     dy(x) = Normal(x, 1)
-#     filename = "data/final/survival-gamma-normal-freq-50.csv"
-#     run_experiment_freq(dy; N0 = 50, Nrep = 5, Niter = 100, id = 1, filename)
-# end 
+# Experiment 1, N0 = 50 (freq)
+begin 
+    dy(x) = LogGamma(4x, 1)
+    filename = "data/final/survival-gamma-loggamma-freq-50.csv"
+    run_experiment_freq(dy; N0 = 50, Nrep = 5, Niter = 100, id = 1, filename)
+end 
 
 # Experiment 1, N0 = 100 (bnp)
 begin 
@@ -171,12 +177,12 @@ begin
     run_experiment_bnp(dy; N0 = 100, Nrep = 5, Niter = 100, id = 1, filename)
 end 
 
-# # Experiment 1, N0 = 100 (freq)
-# begin 
-#     dy(x) = LogNormal(x, 1)
-#     filename = "data/final/survival-gamma-normal-freq-100.csv"
-#     run_experiment_freq(dy; N0 = 100, Nrep = 5, Niter = 100, id = 1, filename)
-# end 
+# Experiment 1, N0 = 100 (freq)
+begin 
+    dy(x) = LogNormal(x, 1)
+    filename = "data/final/survival-gamma-loggamma-freq-100.csv"
+    run_experiment_freq(dy; N0 = 100, Nrep = 5, Niter = 100, id = 1, filename)
+end 
 
 # Experiment 1, N0 = 200 (bnp)
 begin 
@@ -185,52 +191,52 @@ begin
     run_experiment_bnp(dy; N0 = 200, Nrep = 5, Niter = 100, id = 1, filename)
 end 
 
-# # Experiment 1, N0 = 200 (freq)
-# begin 
-#     dy(x) = Normal(x, 1)
-#     filename = "data/final/survival-gamma-normal-freq-200.csv"
-#     run_experiment_freq(dy; N0 = 200, Nrep = 5, Niter = 100, id = 1, filename)
-# end 
+# Experiment 1, N0 = 200 (freq)
+begin 
+    dy(x) = LogGamma(4x, 1)
+    filename = "data/final/survival-gamma-loggamma-freq-200.csv"
+    run_experiment_freq(dy; N0 = 200, Nrep = 5, Niter = 100, id = 1, filename)
+end 
 
-# # Summary of the results
-# R"""
-# data <- 
-#     list.files(
-#         path = "data/final", 
-#         pattern = "survival-gamma*", 
-#         full.names = TRUE
-#     ) |>
-#     purrr::map(readr::read_csv, show_col_types = FALSE) |>
-#     dplyr::bind_rows() |>
-#     dplyr::group_by(method, id, N0, g1, g2, g3, g4, g5, g6) |>
-#     dplyr::count(name = "frequency") |>
-#     dplyr::ungroup() |>
-#     dplyr::mutate(gamma = paste0(g1, g2, g3, g4, g5, g6)) |>
-#     dplyr::select(gamma, frequency, id, method, N = N0)
-# p <- 
-#     data |>
-#     dplyr::mutate(
-#         method = method |>
-#             dplyr::recode(bnp = "BNP", freq = "BSS (Best Subset Selection)"),
-#         distribution = id |>
-#         dplyr::recode(`1` = "Normal", `2` = "Skew Normal"),
-#         N = factor(N, ordered = TRUE)
-#     ) |>
-#     ggplot2::ggplot(ggplot2::aes(y = gamma, x = N, fill = frequency)) +
-#     ggplot2::geom_tile(color = "grey90") +
-#     ggplot2::facet_grid(
-#         distribution ~ method, 
-#         labeller = ggplot2::labeller(
-#             distribution = ggplot2::label_both,
-#             method = ggplot2::label_both
-#         )
-#     ) +
-#     ggplot2::theme_classic() +
-#     ggplot2::theme(legend.position = "top") +
-#     ggplot2::scale_fill_gradient(low = "white", high = "black") + 
-#     ggplot2::labs(
-#         x = "N (sample size)",
-#         y = "gamma (hypothesis vector), as a string of 0s and 1s"
-#     )
-# ggplot2::ggsave("figures/final/heatmap-regression.png")
-# """
+# Summary of the results
+R"""
+data <- 
+    list.files(
+        path = "data/final", 
+        pattern = "survival-gamma*", 
+        full.names = TRUE
+    ) |>
+    purrr::map(readr::read_csv, show_col_types = FALSE) |>
+    dplyr::bind_rows() |>
+    dplyr::group_by(method, id, N0, g1, g2, g3, g4, g5, g6) |>
+    dplyr::count(name = "frequency") |>
+    dplyr::ungroup() |>
+    dplyr::mutate(gamma = paste0(g1, g2, g3, g4, g5, g6)) |>
+    dplyr::select(gamma, frequency, id, method, N = N0)
+p <- 
+    data |>
+    dplyr::mutate(
+        method = method |>
+            dplyr::recode(bnp = "BNP", freq = "BSS (Best Subset Selection)"),
+        distribution = id |>
+        dplyr::recode(`1` = "Normal", `2` = "Skew Normal"),
+        N = factor(N, ordered = TRUE)
+    ) |>
+    ggplot2::ggplot(ggplot2::aes(y = gamma, x = N, fill = frequency)) +
+    ggplot2::geom_tile(color = "grey90") +
+    ggplot2::facet_grid(
+        distribution ~ method, 
+        labeller = ggplot2::labeller(
+            distribution = ggplot2::label_both,
+            method = ggplot2::label_both
+        )
+    ) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "top") +
+    ggplot2::scale_fill_gradient(low = "white", high = "black") + 
+    ggplot2::labs(
+        x = "N (sample size)",
+        y = "gamma (hypothesis vector), as a string of 0s and 1s"
+    )
+ggplot2::ggsave("figures/final/heatmap-survival.png")
+"""
